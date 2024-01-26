8c2e9dbe26cdd220fb4406fd78d410122eabf625
natalie363 <natalie.eckles@mymail.champlain.edu>
2024-01-26 11:19:50 -0500

    Start and stop Google Chrome

---
+++ week2/ProcessManagement4.ps1
@@ -0,0 +1,7 @@
+﻿$web_browser = Get-Process | Where-Object { $_.Name -ilike "chrome" }
+if ($web_browser -eq $null) { 
+    Start-Process -FilePath chrome.exe 'https://champlain.edu'
+}
+else {
+    Stop-Process -Name chrome -Force
+}
