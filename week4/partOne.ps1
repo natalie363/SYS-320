3e25cec900c160115c83832581ab21acccfe7871
natalie363 <natalie.eckles@mymail.champlain.edu>
2024-02-09 12:38:51 -0500

    added comment

--- week4/partOne.ps1
+++ week4/partOne.ps1
@@ -1,3 +1,4 @@
+# this is code from the first part of the assignment, and is not a part of Deliverable 9
 $notFound = Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 '
 
 $unorganizedIP = ForEach-Object {[regex]::Matches($notFound, "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+")}
